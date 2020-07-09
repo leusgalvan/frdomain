04a60db514feef82685e3aba8b8733ea2ca4db07
Leandro Galvan <leusgalvan@gmail.com>
2020-07-09 20:07:27 -0300

    Add Exercise 3.1 solution

--- src/main/scala/frdomain/ch3/algebra/AccountService.scala
+++ src/main/scala/frdomain/ch3/algebra/AccountService.scala
@@ -16,3 +16,35 @@ trait AccountService[Account, Amount, Balance] {
     b <- credit(to, amount)
   } yield (a, b, amount)
 }
+
+trait AccountServiceTry[Account, Amount, Balance] {
+  def open(no: String, name: String, openingDate: Option[Date]): Option[Account]
+  def close(account: Account, closeDate: Option[Date]): Option[Account]
+  def debit(account: Account, amount: Amount): Option[Account]
+  def credit(account: Account, amount: Amount): Option[Account]
+  def balance(account: Account): Option[Balance]
+
+  def transfer(from: Account, 
+               to: Account, 
+               amount: Amount): Option[(Account, Account, Amount)] = 
+    for {
+      a <- debit(from, amount)
+      b <- credit(to, amount)
+    } yield (a, b, amount)
+}
+
+trait AccountServiceEither[Account, Amount, Balance] {
+  def open(no: String, name: String, openingDate: Option[Date]): Either[Throwable, Account]
+  def close(account: Account, closeDate: Option[Date]): Either[Throwable, Account]
+  def debit(account: Account, amount: Amount): Either[Throwable, Account]
+  def credit(account: Account, amount: Amount): Either[Throwable, Account]
+  def balance(account: Account): Either[Throwable, Balance]
+
+  def transfer(from: Account, 
+               to: Account, 
+               amount: Amount): Either[Throwable, (Account, Account, Amount)] = 
+    for {
+      a <- debit(from, amount).right
+      b <- credit(to, amount).right
+    } yield (a, b, amount)
+}
